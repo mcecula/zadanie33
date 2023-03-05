edf307110417619958db0f7e91b2fd8c4f82108e
mcecula <mc23051988@gmail.com>
2023-03-05 20:28:26 +0100

    zadanie33

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let zmienna = document.getElementById("opis");
+
+let button = document.querySelector('button').onclick = function () {
+    alert(zmienna + 'dowolny tekst')
+};
+console.log(zmienna)
