ea9e9143acea3b62a3535971a8f50f532f0df10f
mcecula <mc23051988@gmail.com>
2023-03-09 12:23:16 +0100

    poprawka

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let zmienna = document.getElementById("opis")
 
 let button = document.querySelector('button').onclick = function () {
-    alert(zmienna.textContent = 'Mariusz')
+    zmienna.textContent = 'Mariusz'
 }
